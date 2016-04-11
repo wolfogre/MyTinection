769ffd1cbc13dad2fe3de46224892c5a9b22dad8
Jason Song <wolfogre@outlook.com>
2016-04-12 01:48:23 +0800

    modified:   page-software.php deleted:    "postviews to tracker/\313\265\303\367.txt"

--- page-software.php
+++ page-software.php
@@ -9,12 +9,12 @@ Template Name: 软件中心
  *
  * @package   Tinection
  * @version   1.1.5
- * @date      2015.1.28
- * @author    Zhiyan <chinash2010@gmail.com>
- * @site      Zhiyanblog <www.zhiyanblog.com>
- * @copyright Copyright (c) 2014-2015, Zhiyan
+ * @date      2016.4.12
+ * @author    Jason Song <wolfogre@outlook.com>
+ * @site      WolfogreBlog <blog.wolfogre.com>
+ * @copyright Copyright Unreserved
  * @license   http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
- * @link      http://www.zhiyanblog.com/tinection.html
+ * @link      http://blog.wolfogre.com
 **/
 
 ?>
